c5f2d836e85fe205f756611ad8da8d89d1ab2488
YihaoPeng <yihao.peng@bitmain.com>
2018-08-21 20:10:29 +0800

    mklink.bat: update dir

--- mklink.bat
+++ mklink.bat
@@ -1,2 +1,2 @@
-mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\include\btctools" "C:\work\win32\libbtctools\src"
+mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\include\btctools" "C:\work\win32\libbtctools\src"
 pause
